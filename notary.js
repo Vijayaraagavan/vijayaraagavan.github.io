4fcddda6d04032142cd0d5000ad1e383dcdc20d0
Vijayaraagavan <96820450+Vijayaraagavan@users.noreply.github.com>
2022-01-04 19:22:31 +0530

    Update notary.js

--- notary.js
+++ notary.js
@@ -1,6 +1,6 @@
 function loadbook(filename, displayName){
     let currentBook = "";
-    let url = "https://notary/" + filename;
+    let url = "https://" + filename;
 
 //reset our UI
 document.getElementById("filename").innerHTML = displayName;
